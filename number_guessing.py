5b86c471fb4fafed3fb279d181ddb391667c3c65
yf2416 <git config --global user.email>
2026-06-16 00:42:19 +0800

    Completed

--- number_guessing.py
+++ number_guessing.py
@@ -5,6 +5,7 @@ Your task is to complete the incomplete function definition. so that it behaves 
 Do not run this file directly.
 Rather, call this function from main.py and run that file.
 """
+import random
 
 
 def guess_number(low, high, num_attempts):
@@ -24,3 +25,20 @@ def guess_number(low, high, num_attempts):
     :param num_attempts: The number of attempts the user is given to guess the correct number.
     :returns: True if the user answers any attempt correctly, False otherwise.
     """
+    target = random.randint(low,high)
+    print(f"You have {num_attempts} of chances to guess the number between {low} and {high}! ")
+    for attempt in range(num_attempts):
+        guess = input(f"Guess a number between {low} and {high}: ")
+        if not guess.isdigit():
+            print("You must enter a valid number.")
+        elif int(guess) == target:
+            return True
+            break 
+        else:
+            print("Pleaase try again")
+
+    print("You have used up all your attempts")
+    return False
+
+
+
--- virus_transmission.py
+++ virus_transmission.py
@@ -17,3 +17,9 @@ def calculate_infections(starting_number_infections, reproduction_rate, num_days
     :param reproduction_rate: A floating point number indicating the rate of growth in the number of infections each day.
     :returns: The new number of infected individuals after the given number of days, rounded to the nearest integer.
     """
+    infection = starting_number_infections
+
+    for day_num in range(num_days):
+        infection = infection * reproduction_rate
+
+    return round(infection)
